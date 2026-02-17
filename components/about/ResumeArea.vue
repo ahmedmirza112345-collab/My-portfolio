3810780499020715655a50d530f5b78638c5a609
Bilal Ahmed Mirza <ahmed.mirza112345@gmail.com>
2026-02-17 23:01:05 +0500

    Fixed Elementor link and matched all correct icons

--- components/about/ResumeArea.vue
+++ components/about/ResumeArea.vue
@@ -1,66 +1,61 @@
- 
- 
- <template>
-<div class="resume-area no-padding" id="resume">
-        <div class="container">
-          <div class="row">
-            <div class="col-xl-6 col-md-6">
-              <div class="resume-wrapper wow fadeInUp delay-0-2s">
+<template>
+  <div class="resume-area no-padding" id="resume">
+    <div class="container">
+      <div class="row">
+        <div class="col-xl-6 col-md-6">
+          <div class="resume-wrapper wow fadeInUp delay-0-2s">
 
-                <div class="resume-box">
-                  <span class="resume-date">2020 - Present</span>
-                  <h2>Web Developer & Business Partner (Full-Time)</h2>
-                  <span>@ Envato Marketplace</span>
-                  <p>Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                </div>
-
-
-                <div class="resume-box">
-                  <span class="resume-date">2018 - 2020</span>
-                  <h2>CEO & Founder</h2>
-                  <span>@ Thememaster LTD</span>
-                  <p>Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                </div>
-
-
-                <div class="resume-box">
-                  <span class="resume-date">2012 - 2028</span>
-                  <h2>Senior Designer</h2>
-                  <span>@ Google</span>
-                  <p>Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                </div>
-
-              </div>
+            <div class="resume-box">
+              <span class="resume-date">May 2025 - Present</span>
+              <h2>Creative Lead & Operations Manager</h2>
+              <span>@ Squarestech (On-site, Gujrat)</span>
+              <p>Managing on-site operations and multidisciplinary teams. Solely designed and developed the official company website using Figma, WordPress, and Elementor. Architecting automation workflows in GoHighLevel.</p>
             </div>
-            <div class="col-xl-6 col-md-6">
-              <div class="resume-wrapper wow fadeInUp delay-0-4s">
 
-                <div class="resume-box">
-                  <span class="resume-date">2020 - Present</span>
-                  <h2>Bachelor Degree of Science</h2>
-                  <span>@ University of London</span>
-                  <p>Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                </div>
-
-
-                <div class="resume-box">
-                  <span class="resume-date">2018 - 2020</span>
-                  <h2>Master Degree of Design</h2>
-                  <span>@ Envato Education</span>
-                  <p>Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                </div>
-
-
-                <div class="resume-box">
-                  <span class="resume-date">2015 - 2018</span>
-                  <h2>Degree Of UI & UX</h2>
-                  <span>@ University of world</span>
-                  <p>Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                </div>
-
-              </div>
+            <div class="resume-box">
+              <span class="resume-date">May 2025 - Present</span>
+              <h2>Remote Creative Lead</h2>
+              <span>@ Techspace (Remote, Norway)</span>
+              <p>Providing high-level visual identity and web design solutions for the Scandinavian market. Developing minimalist, high-impact branding ecosystems that align with Nordic design principles.</p>
             </div>
+
+            <div class="resume-box">
+              <span class="resume-date">March 2022 - March 2025</span>
+              <h2>Graphic Designer</h2>
+              <span>@ Digitron Solutionz (On-site, Gujrat)</span>
+              <p>Designed large-format print media and digital assets for high-volume local market advertising strategies. Provided essential video editing support for social media reels using Premiere Pro and CapCut.</p>
+            </div>
+
+          </div>
+        </div>
+        
+        <div class="col-xl-6 col-md-6">
+          <div class="resume-wrapper wow fadeInUp delay-0-4s">
+
+            <div class="resume-box">
+              <span class="resume-date">Degree</span>
+              <h2>Intermediate in Computer Science (ICS)</h2>
+              <span>@ Royal National College, Kharian</span>
+              <p>Completed foundational studies in computer science, developing the technical mindset needed to bridge the gap between creative visual design and technical web execution.</p>
+            </div>
+
+            <div class="resume-box">
+              <span class="resume-date">Certification</span>
+              <h2>CRM Automation Specialist</h2>
+              <span>@ GoHighLevel (GHL)</span>
+              <p>Self-directed learning and practical application in GoHighLevel. Specialized in architecting workflows, automating client onboarding, and building scalable lead nurturing pipelines.</p>
+            </div>
+
+            <div class="resume-box">
+              <span class="resume-date">Certification (In Progress)</span>
+              <h2>Google Ads Search Certification</h2>
+              <span>@ Google / Self Learning</span>
+              <p>Continuous professional development focused on search engine marketing, driving revenue through paid ad campaigns, and optimizing A/B test creatives for maximum engagement.</p>
+            </div>
+
           </div>
         </div>
       </div>
- </template>
+    </div>
+  </div>
+</template>
--- components/about/SkillArea.vue
+++ components/about/SkillArea.vue
@@ -1,72 +1,91 @@
-
 <template>
   <section id="skills" class="skill-area">
-        <div class="container">
-          <div class="container-inner">
-            <div class="row">
-              <div class="col-xl-12 col-lg-12">
-                <div class="section-title section-black-title mb-40 wow fadeInUp delay-0-2s">
-                  <h2>Professional Skills</h2>
-                </div>
-              </div>
+    <div class="container">
+      <div class="container-inner">
+        <div class="row">
+          <div class="col-xl-12 col-lg-12">
+            <div class="section-title section-black-title mb-40 wow fadeInUp delay-0-2s">
+              <h2>Professional Skills</h2>
             </div>
-            <div class="row">
-              <div class="col-lg-12">
-                <div class="skill-items-wrap">
-                  <div class="row">
-                    <div class="col-xl-3 col-lg-4 col-md-3 col-sm-4 col-6">
-                      <div class="skill-item wow fadeInUp delay-0-2s">
-                        <img src="assets/images/skills/skill1.png" alt="Skill" />
-                        <h5>Figma</h5>
-                      </div>
-                    </div>
-                    <div class="col-xl-3 col-lg-4 col-md-3 col-sm-4 col-6">
-                      <div class="skill-item wow fadeInUp delay-0-3s">
-                        <img src="assets/images/skills/skill2.png" alt="Skill" />
-                        <h5>Tailwand</h5>
-                      </div>
-                    </div>
-                    <div class="col-xl-3 col-lg-4 col-md-3 col-sm-4 col-6">
-                      <div class="skill-item wow fadeInUp delay-0-4s">
-                        <img src="assets/images/skills/skill3.png" alt="Skill" />
-                        <h5>Photoshop</h5>
-                      </div>
-                    </div>
-                    <div class="col-xl-3 col-lg-4 col-md-3 col-sm-4 col-6">
-                      <div class="skill-item wow fadeInUp delay-0-5s">
-                        <img src="assets/images/skills/skill4.png" alt="Skill" />
-                        <h5>WordPress</h5>
-                      </div>
-                    </div>
-                    <div class="col-xl-3 col-lg-4 col-md-3 col-sm-4 col-6">
-                      <div class="skill-item wow fadeInUp delay-0-2s">
-                        <img src="assets/images/skills/skill5.png" alt="Skill" />
-                        <h5>Angular</h5>
-                      </div>
-                    </div>
-                    <div class="col-xl-3 col-lg-4 col-md-3 col-sm-4 col-6">
-                      <div class="skill-item wow fadeInUp delay-0-3s">
-                        <img src="assets/images/skills/skill6.png" alt="Skill" />
-                        <h5>Webflow</h5>
-                      </div>
-                    </div>
-                    <div class="col-xl-3 col-lg-4 col-md-3 col-sm-4 col-6">
-                      <div class="skill-item wow fadeInUp delay-0-4s">
-                        <img src="assets/images/skills/skill7.png" alt="Skill" />
-                        <h5>Python</h5>
-                      </div>
-                    </div>
-                    <div class="col-xl-3 col-lg-4 col-md-3 col-sm-4 col-6">
-                      <div class="skill-item wow fadeInUp delay-0-5s">
-                        <img src="assets/images/skills/skill8.png" alt="Skill" />
-                        <h5>Sketch</h5>
-                      </div>
-                    </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="skill-items-wrap">
+              <div class="row">
+                
+                <div class="col-xl-3 col-lg-4 col-md-3 col-sm-4 col-6">
+                  <div class="skill-item wow fadeInUp delay-0-2s">
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/figma/figma-original.svg" alt="Figma" class="official-icon" />
+                    <h5>Figma</h5>
                   </div>
                 </div>
+                
+                <div class="col-xl-3 col-lg-4 col-md-3 col-sm-4 col-6">
+                  <div class="skill-item wow fadeInUp delay-0-3s">
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/wordpress/wordpress-plain.svg" alt="WordPress" class="official-icon" />
+                    <h5>WordPress</h5>
+                  </div>
+                </div>
+                
+                <div class="col-xl-3 col-lg-4 col-md-3 col-sm-4 col-6">
+                  <div class="skill-item wow fadeInUp delay-0-4s">
+                    <img src="https://www.svgrepo.com/show/353685/elementor.svg" alt="Elementor" class="official-icon" />
+                    <h5>Elementor</h5>
+                  </div>
+                </div>
+                
+                <div class="col-xl-3 col-lg-4 col-md-3 col-sm-4 col-6">
+                  <div class="skill-item wow fadeInUp delay-0-5s">
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/photoshop/photoshop-original.svg" alt="Photoshop" class="official-icon" />
+                    <h5>Photoshop</h5>
+                  </div>
+                </div>
+                
+                <div class="col-xl-3 col-lg-4 col-md-3 col-sm-4 col-6">
+                  <div class="skill-item wow fadeInUp delay-0-2s">
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/illustrator/illustrator-original.svg" alt="Illustrator" class="official-icon" />
+                    <h5>Illustrator</h5>
+                  </div>
+                </div>
+                
+                <div class="col-xl-3 col-lg-4 col-md-3 col-sm-4 col-6">
+                  <div class="skill-item wow fadeInUp delay-0-3s">
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/premierepro/premierepro-original.svg" alt="Premiere Pro" class="official-icon" />
+                    <h5>Premiere Pro</h5>
+                  </div>
+                </div>
+                
+                <div class="col-xl-3 col-lg-4 col-md-3 col-sm-4 col-6">
+                  <div class="skill-item wow fadeInUp delay-0-4s">
+                    <img src="https://avatars.githubusercontent.com/u/55125324?s=200&v=4" alt="GoHighLevel" class="official-icon" style="border-radius: 12px;" />
+                    <h5>GoHighLevel</h5>
+                  </div>
+                </div>
+                
+                <div class="col-xl-3 col-lg-4 col-md-3 col-sm-4 col-6">
+                  <div class="skill-item wow fadeInUp delay-0-5s">
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/facebook/facebook-original.svg" alt="Meta Ads" class="official-icon" />
+                    <h5>Meta Ads</h5>
+                  </div>
+                </div>
+                
               </div>
             </div>
           </div>
         </div>
-      </section>
+      </div>
+    </div>
+  </section>
 </template>
+
+<style scoped>
+/* Forces all the different logos to be the exact same size */
+.official-icon {
+  width: 55px;
+  height: 55px;
+  object-fit: contain;
+  margin-bottom: 20px;
+  display: inline-block;
+}
+</style>
